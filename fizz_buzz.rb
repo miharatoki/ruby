ca66548d9817a064773b30dfc25a1217d29219f8
EC2 Default User <ec2-user@ip-172-31-23-235.ec2.internal>
2021-06-27 03:34:02 +0000

    (finish)2-8

---
+++ fizz_buzz.rb
@@ -0,0 +1,37 @@
+def fizz_buzz(number)
+  if number%5==0 && number%3==0
+    puts "FizzBuzz"
+  elsif number%3==0
+    puts "Fizz"
+  elsif number%5==0
+    puts "Buzz"
+  else number
+  end
+end
+
+puts "数字を入力してください。"
+
+input = gets.to_i
+
+puts "結果は..."
+puts fizz_buzz(input)
+
+
+# def fizz_buzz(number)
+#   if number % 15 == 0
+#     "FizzBuzz"
+#   elsif number % 3 == 0
+#     "Fizz"
+#   elsif number % 5 == 0
+#     "Buzz"
+#   else
+#     number.to_s
+#   end
+# end
+
+# puts "数字を入力してください。"
+
+# input = gets.to_i
+
+# puts '結果は...'
+# puts fizz_buzz(input)
